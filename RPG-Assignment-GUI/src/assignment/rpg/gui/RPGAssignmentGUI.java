3aa9a019b18481a7be2ba6c411493fe11490a123
Jonathan Holmes <pcf1235@aut.ac.nz>
2017-03-17 11:00:10 +1300

    Added GUI project for later on.

---
+++ RPG-Assignment-GUI/src/assignment/rpg/gui/RPGAssignmentGUI.java
@@ -0,0 +1,16 @@
+package assignment.rpg.gui;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class RPGAssignmentGUI
+{
+/**
+ * @param args the command line arguments
+ */
+    public static void main(String[] args)
+    {
+	// TODO code application logic here
+    }
+}
