c09a5c504913439f9ff45875292798277d02a0b2
Vorlias <void.knight09@gmail.com>
2017-03-16 18:41:34 +1300

    Updated project to use tabs and newline braces.
    Good to force the project to use them then to try and have the hassle of
    setting up Nathan's IDE to work the same way.

---
+++ RPG-Assignment-CUI/src/assignment/rpg/cui/RPGAssignmentCUI.java
@@ -0,0 +1,16 @@
+package assignment.rpg.cui;
+
+/**
+ *
+ * @author Jonathan Holmes & Nathan Cross
+ */
+public class RPGAssignmentCUI
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+	System.out.println("Hello, World!");
+    }
+}
