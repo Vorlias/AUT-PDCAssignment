528b86efd287aa92190327a82839d6b726f7303d
Jonathan Holmes <pcf1235@aut.ac.nz>
2017-04-05 12:48:54 +1200

    Added random range methods to Utility class

--- RPG-Assignment-CUI/src/rpg/cui/misc/Utility.java
+++ RPG-Assignment-CUI/src/rpg/cui/misc/Utility.java
@@ -5,12 +5,16 @@
  */
 package rpg.cui.misc;
 
+import java.util.Random;
+
 /**
  *
  * @author Nathan
  */
 public class Utility
 {
+	static Random random = new Random();
+	
 	/**
 	 * Print a specified amount of a character to console
 	 */
@@ -21,5 +25,27 @@ public class Utility
 			System.out.print(character);
 		}
 		System.out.println();
+	}
+	
+	/**
+	 * Returns a random float between min and max
+	 * @param min The minimum value
+	 * @param max The maximum value
+	 * @return The random number
+	 */
+	public static float randomFloat(float min, float max)
+	{
+		return random.nextFloat() * (max - min) + min;
+	}	
+	
+	/**
+	 * Returns a random integer between min and max
+	 * @param min The minimum value
+	 * @param max The maximum value
+	 * @return The random number
+	 */
+	public static int randomInt(int min, int max)
+	{
+		return random.nextInt((max - min) + 1) + min;
 	}
 }
