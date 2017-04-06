f4d88c0ff2705f16afb172b726950d3e2a22b1c3
Vorlias <void.knight09@gmail.com>
2017-04-06 20:13:07 +1200

    Added inventory interaction

--- RPG-Assignment-CUI/src/rpg/cui/game/Inventory.java
+++ RPG-Assignment-CUI/src/rpg/cui/game/Inventory.java
@@ -6,9 +6,12 @@
 package rpg.cui.game;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import rpg.cui.characters.PlayerCharacter;
 import rpg.cui.items.Item;
+import rpg.cui.items.Consumable;
+import rpg.cui.items.Weapon;
 
 /**
  *
@@ -23,15 +26,6 @@ public class Inventory
 	 */
 	public static void handleInventory()
 	{
-		PlayerCharacter character = Game.getPlayerCharacter();
-		ArrayList<Item> items = character.getItems();
-		
-		System.out.println("=== Inventory ===");
-		for (int i = 0; i < items.size(); i++)
-		{
-			Item item = items.get(i);
-			System.out.println((i + 1) + ". " + item.getName());
-		}
 		
 		handleInventoryOptions();
 	}
@@ -41,6 +35,79 @@ public class Inventory
 	 */
 	private static void handleInventoryOptions()
 	{
+		PlayerCharacter character = Game.getPlayerCharacter();
+		ArrayList<Item> items = character.getItems();
 		
+		
+		System.out.println("System: You open your inventory, these are the items you have:");
+		
+		System.out.println("\t-- Your Inventory --");
+		for (int i = 0; i < items.size(); i++)
+		{
+			Item item = items.get(i);
+			System.out.println("\t   " + (i + 1) + ". " + item.getName());
+		}
+		
+		System.out.println("System: What would you like to do?");
+		System.out.println("	use <index> - equip the item");
+		System.out.println("	close - close your inventory");
+		
+		String action = "";
+		do 
+		{
+			try
+			{
+				System.out.print("Inventory> ");
+				action = scanner.next().toLowerCase();
+				switch (action)
+				{
+					case "use":
+						int id = scanner.nextInt() - 1;
+						if (id >= 0 && id < items.size())
+						{
+							Item item = items.get(id);
+							if (item != null)
+							{
+								if (item instanceof Weapon)
+								{
+									character.equipItem(item);
+									System.out.println("Equipped " + item.getName());
+								}
+								else if (item instanceof Consumable)
+								{
+									item.use(character);
+									System.out.println("Used " + item.getName());
+								}
+							}
+						}
+						
+						break;
+					case "close":
+						break;
+					default:
+						System.out.println("Unknown command '" + action + "'");
+				}				
+			}
+			catch (InputMismatchException e)
+			{
+				System.out.println("Invalid input entered, try again.");
+			}
+		}
+		while (!action.equals("close"));
+		
+		System.out.println("System: You close your inventory.");
+		
+		if (Game.playerInForest)
+		{
+			Explore.handleExplore();
+		}
+		else if (Game.playerInTown)
+		{
+			Town.handleTown();
+		}
+		else
+		{
+			Game.chooseNextLocation();
+		}
 	}
 }
