33032834b266fb2cc6f01cb0eca0aece8c5c4ff7
Vorlias <void.knight09@gmail.com>
2017-05-05 01:55:10 +1200

    Added game states

---
+++ RPG-Assignment-GUI/src/rpg/gui/states/PlayState.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpg.gui.states;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+import rpg.gui.RPGGame;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class PlayState extends BasicGameState
+{
+
+    @Override
+    public int getID()
+    {
+	return RPGGame.STATE_GAME;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException
+    {
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException
+    {
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException
+    {
+    }
+    
+}
