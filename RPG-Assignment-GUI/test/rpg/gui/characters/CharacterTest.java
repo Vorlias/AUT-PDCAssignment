4f0e90063ed8e6f41580bdc251cf38e990a44ae8
Nate Cross <buffynath_10@hotmail.com>
2017-06-06 14:14:03 +1200

    Commented Character Test Class

--- RPG-Assignment-GUI/test/rpg/gui/characters/CharacterTest.java
+++ RPG-Assignment-GUI/test/rpg/gui/characters/CharacterTest.java
@@ -14,56 +14,94 @@ import static org.junit.Assert.*;
  */
 public class CharacterTest
 {
-    private String testName;
-    private int testLevel;
-    private int testGold;
-    private float testHealth;
+    private String testName; // Character name
+    private int testLevel; // Character level
+    private int testGold; // Character gold
+    private float testHealth; // Character health
     
+    /**
+     * Character constructor
+     */
     public CharacterTest()
     {
 	
     }
 
+    /**
+     * Gets the name of the character
+     * @return the character test name
+     */
     public String getTestName()
     {
 	return testName;
     }
 
+    /**
+     * Sets the name of the character
+     * @param testName the name to set to the character
+     */
     public void setTestName(String testName)
     {
 	this.testName = testName;
     }
 
+    /**
+     * Gets the level of the character
+     * @return the character level
+     */
     public int getTestLevel()
     {
 	return testLevel;
     }
 
+    /**
+     * Sets the level of the character
+     * @param testLevel the level to set the character to
+     */
     public void setTestLevel(int testLevel)
     {
 	this.testLevel = testLevel;
     }
 
+    /**
+     * Gets the gold the character has
+     * @return the amount of gold
+     */
     public int getTestGold()
     {
 	return testGold;
     }
 
+    /**
+     * Set the gold the character has
+     * @param testGold the gold to set to the character
+     */
     public void setTestGold(int testGold)
     {
 	this.testGold = testGold;
     }
 
+    /**
+     * Get the health the character has
+     * @return the amount of health
+     */
     public float getTestHealth()
     {
 	return testHealth;
     }
 
+    /**
+     * Sets the health the character has
+     * @param testHealth the amount of health to set
+     */
     public void setTestHealth(float testHealth)
     {
 	this.testHealth = testHealth;
     }
     
+    /**
+     * Test setting character name
+     */
     @Test
     public void testSetName()
     {
@@ -76,6 +114,9 @@ public class CharacterTest
 	assertEquals(expectedName, actualName);
     }
     
+    /**
+     * Test setting character level
+     */
     @Test
     public void testSetLevel()
     {
@@ -88,6 +129,9 @@ public class CharacterTest
 	assertEquals(expectedLevel, actualLevel);
     }
     
+    /**
+     * Test setting character gold
+     */
     @Test
     public void testSetGold()
     {
@@ -100,6 +144,9 @@ public class CharacterTest
 	assertEquals(expectedGold, actualGold);
     }
     
+    /**
+     * Test setting character health
+     */
     @Test
     public void testSetHealth()
     {
